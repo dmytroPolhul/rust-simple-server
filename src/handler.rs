42aeb2c329eec1a84a2a740b35bc06a577f4ceb0
Dmytro Polhul <dmytro.polhul@fivesysdev.com>
2023-06-01 20:05:11 +0300

    simple pure rust one tread server

---
+++ src/handler.rs
@@ -0,0 +1,46 @@
+use super::server::Handler;
+use std::fs;
+use super::http::{Request, Response, StatusCode, Method};
+
+
+pub struct WebHandler {
+    public_path: String
+}
+
+impl WebHandler {
+    pub fn new(public_path: String) -> Self {
+        Self {public_path}
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory error");
+                    None
+                }
+            },
+            Err(_) => None
+        }
+    }
+}
+
+impl Handler for WebHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Test HELLO GET</h1>".to_string())),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None =>Response::new(StatusCode::NotFound, None),
+                },
+            }
+            _ => Response::new(StatusCode::NotFound, None),
+        }
+    }
+}
